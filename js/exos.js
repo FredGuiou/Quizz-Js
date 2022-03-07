feb279120ef5da79f0a585ef29b68bb4be9de4ec
FredGuiou <frederic.guiou@gmail.com>
2022-03-07 17:06:59 +0100

    Challenge acompli ex1-2-3 & bonus

--- js/exos.js
+++ js/exos.js
@@ -5,11 +5,33 @@
 * Et écris ton code ici même !
 */
 
+var score = 0;
+
 /* Exo 1 */
-// Ton code ici...
+var question1 = "Quelle mer borde la ville de Sébastopol ?";
+var solution1 = "la mer Noire";
 
 /* Exo 2 */
-// Ton code ici...
+var reponse1 = prompt(question1);
+if (reponse1===solution1){
+    alert("Gagné !");
+    score++
+} else {
+    alert("Perdu...")
+}
 
 /* Exo 3 */
-// Ton code ici...
+var question2 = "Quel est l'âge du capitaine ?";
+var solution2 = 63;
+
+var reponse2 = prompt (question2);
+reponse2 = parseInt(reponse2);
+
+if (reponse2 === solution2) {
+    alert ("Gagné");
+    score++
+} else {
+    alert ("Perdu...");
+}
+
+alert("Vous avez "+score+" bonne(s) réponse(s)")
